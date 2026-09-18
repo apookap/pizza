9d3697888bfbfd28aa816d81fb7bb936913db4aa
apookap <apoorvak@gmail.com>
2026-09-18 09:03:37 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pizza-app-v2';
+const assets = [
+  'pizza.html',
+  // Add any CSS or images your page uses here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
